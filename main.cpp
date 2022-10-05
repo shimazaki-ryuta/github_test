d9e90e0e26da5d8fd53267655c933e6f02b24487
嶋﨑龍太 <@MYPC01>
2022-10-05 11:53:23 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+int main() {
+	int end_flg = 0;
+
+	std::cout<< "hello world\n";
+
+	printf("\nコンソールアプリケーションを終了する場合は数字の1を入力しEnterキーを押してください");
+	scanf_s("%d",&end_flg);
+	if (end_flg == 1)return 0;
+	else return -1;
+}
